ad081df94d614054756cc8907e86539cf4567937
Fransaviour <122869087+Fransaviour@users.noreply.github.com>
2023-10-06 17:07:48 +0300

    Update precision.c

--- precision.c
+++ precision.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
+ * get_precision - Calculate precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * @i: List of args to be printed.
  * @list: list of arguments.
  *
  * Return: Precision.
